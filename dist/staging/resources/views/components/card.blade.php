2932090fd77959de66310032d90b20e1fd5c3f1b
rvymnl <pineda.raymanuel@gmail.com>
2026-09-13 15:50:54 +0800

    chore: untrack dist/ (release build artifact), ignore tool junk

--- dist/staging/resources/views/components/card.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['padding' => 'p-6'])
-
-<div {{ $attributes->merge(['class' => "bg-white dark:bg-slate-800 border border-slate-200/70 dark:border-slate-700/80 rounded-2xl shadow-sm {$padding}"]) }}>
-    {{ $slot }}
-</div>
